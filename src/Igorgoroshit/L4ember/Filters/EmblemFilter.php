493eabd7b88a2ee0d5be6841122c134e867c5197
Igor Goroshit <igor@webt.co.il>
2015-01-05 04:25:07 +0200

    'updates'

---
+++ src/Igorgoroshit/L4ember/Filters/EmblemFilter.php
@@ -0,0 +1,37 @@
+<?php namespace Igorgoroshit\L4ember\Filters;
+
+use Assetic\Asset\AssetInterface;
+use Assetic\Filter\FilterInterface;
+use Codesleeve\AssetPipeline\Filters\FilterHelper;
+
+class EmblemFilter extends FilterHelper implements FilterInterface 
+{
+    public function __construct($basePath = '/app/assets/javascripts/')
+    {
+        $this->basePath = $basePath;
+    }
+
+    public function filterLoad(AssetInterface $asset)
+    {
+    }
+ 
+    public function filterDump(AssetInterface $asset)
+    {
+        $relativePath = ltrim($this->getRelativePath($this->basePath, $asset->getSourceRoot() . '/'), '/');
+        $filename =  pathinfo($asset->getSourcePath(), PATHINFO_FILENAME);
+        
+        $filename = pathinfo($filename, PATHINFO_FILENAME);
+        
+        $dirname = explode("templates/", dirname($relativePath . $filename) . '/');
+        $parent_dir = ((count($dirname) > 0) ? $dirname[1] : "");
+
+        $content = str_replace('"', '\\"', $asset->getContent());
+        $content = str_replace(PHP_EOL, "\\n", $content);
+        
+        $emblem = 'Ember.TEMPLATES["' . $parent_dir . $filename . '"] = Emblem.compile(Ember.Handlebars, "';
+        $emblem .= $content;
+        $emblem .= '");' . PHP_EOL;
+
+        $asset->setContent($emblem);
+    }
+}
---
+++ src/Igorgoroshit/L4ember/L4emberServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace Igorgoroshit\L4ember;
+
+use Illuminate\Support\ServiceProvider;
+
+class L4emberServiceProvider extends ServiceProvider {
+
+
+	protected $defer = false;
+
+
+	public function boot()
+	{
+		//print_r($this->app);die();
+		$replace_once = 1;
+		$project_base = $this->app['path'];
+		//die($project_base . '/assets');
+		$base = realpath($project_base . '/assets');
+		//die($base);
+		$base = str_replace($project_base, '', $base, $replace_once);
+		$base = ltrim($base, '/');
+		//die($project_base);
+		\Event::listen('asset.pipeline.boot', function($pipeline) use ($base) {
+			
+			$config = $pipeline->getConfig();
+			$config['paths'][] = $base . '/javascripts';
+			$config['paths'][] = $base . '/stylesheets';
+			$config['mimes']['javascripts'][] = '.emblem';
+
+			$config['filters']['.emblem'] = array(
+				new Filters\EmblemjsFilter($config['paths'])
+			);
+
+			$pipeline->setConfig($config);
+
+		});
+
+		$this->package('igorgoroshit/l4ember');
+	}
+
+
+	public function register(){}
+
+	public function provides(){ return array(); }
+
+}
